9b84766367237db17efd1fab1c3d4fc4f9c2ecf0
nolauraen <n40943802@gmail.com>
2026-05-15 01:28:50 +0200

    script.js aktualisieren

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function scrollToSection() {
-  document.getElementById("models").scrollIntoView({
-    behavior: "smooth"
-  });
+function transform(btn) {
+  const card = btn.parentElement;
+  const img = card.querySelector("img");
+
+  if (img.dataset.state === "robot") {
+    img.src = img.dataset.car;
+    img.dataset.state = "car";
+  } else {
+    img.src = img.dataset.robot;
+    img.dataset.state = "robot";
+  }
 }
