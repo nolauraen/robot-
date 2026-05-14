cb2325be2c86e9173fc272c88a252fc329ba7676
nolauraen <n40943802@gmail.com>
2026-05-15 01:34:38 +0200

    script.js aktualisieren

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ function transform(btn) {
   const card = btn.parentElement;
   const img = card.querySelector("img");
 
-  if (img.dataset.state === "robot") {
-    img.src = img.dataset.car;
-    img.dataset.state = "car";
-  } else {
-    img.src = img.dataset.robot;
-    img.dataset.state = "robot";
-  }
+  // эффект “исчезновения”
+  img.style.opacity = "0";
+  img.style.transform = "scale(0.8) rotate(5deg)";
+
+  setTimeout(() => {
+    if (img.dataset.state === "robot") {
+      img.src = img.dataset.car;
+      img.dataset.state = "car";
+    } else {
+      img.src = img.dataset.robot;
+      img.dataset.state = "robot";
+    }
+
+    // эффект появления
+    img.style.opacity = "1";
+    img.style.transform = "scale(1) rotate(0deg)";
+  }, 300);
+}
+
+function scrollToSection() {
+  document.getElementById("models").scrollIntoView({
+    behavior: "smooth"
+  });
 }
