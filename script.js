f08ccf8125fe0bba2d7a4cdbd66c01ec6fcae527
nolauraen <n40943802@gmail.com>
2026-05-15 02:06:57 +0200

    script.js aktualisieren

--- script.js
+++ script.js
@@ -2,22 +2,11 @@ function transform(btn) {
   const card = btn.parentElement;
   const img = card.querySelector("img");
 
-  // эффект “исчезновения”
   img.style.opacity = "0";
-  img.style.transform = "scale(0.8) rotate(5deg)";
 
   setTimeout(() => {
-    if (img.dataset.state === "robot") {
-      img.src = img.dataset.car;
-      img.dataset.state = "car";
-    } else {
-      img.src = img.dataset.robot;
-      img.dataset.state = "robot";
-    }
-
-    // эффект появления
+    img.src = "https://via.placeholder.com/300x200.png?text=ROBOT+MODE";
     img.style.opacity = "1";
-    img.style.transform = "scale(1) rotate(0deg)";
   }, 300);
 }
 
